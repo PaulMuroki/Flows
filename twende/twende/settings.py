4503337e9c11614430d46ec8610463b3e89505e4
Elias Kioni <eliaskioni94@gmail.com>
2023-10-04 13:02:47 +0000

    add

--- twende/twende/settings.py
+++ twende/twende/settings.py
@@ -78,11 +78,12 @@ WSGI_APPLICATION = "twende.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
